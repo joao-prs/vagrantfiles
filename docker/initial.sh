7ce1be32aa39a9ef493c8638d725a56a809a587e
joao <kandrocmatunein@gmail.com>
2022-10-06 10:08:58 -0300

    testes docker

--- docker/initial.sh
+++ docker/initial.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 apt update -y
 apt upgrade -y
-apt dist-upgrade -y
+#apt dist-upgrade -y
 apt install -y docker.io docker-compose
 sudo systemctl enable docker
 sudo systemctl start docker
