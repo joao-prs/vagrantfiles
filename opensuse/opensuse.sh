4800865dada24fd91d38976ec8bb0d7a8df3e148
joao <kandrocmatunein@gmail.com>
2023-05-08 14:06:05 -0300

    change version

---
+++ opensuse/opensuse.sh
@@ -0,0 +1,22 @@
+# sudo zypper ar http://download.opensuse.org/repositories/utilities/openSUSE_Leap_15.4/ Utilities
+# sudo zypper in neofetch
+
+# zypper repos --uri
+
+# sudo zypper refresh
+# or
+# sudo zypper ref
+
+# sudo zypper list-patches
+# or
+# sudo zypper lp
+
+# sudo zypper list-updates
+# or
+# sudo zypper lu
+
+# sudo zypper update -y
+# or
+# sudo zypper up
+
+
