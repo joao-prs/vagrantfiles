8f897b137fa000e83d1978d5892191281c4d606c
joao <kandrocmatunein@gmail.com>
2023-05-30 17:30:09 -0300

    update mariadb

--- mariadb-pub/initial.sh
+++ mariadb-pub/initial.sh
@@ -21,12 +21,10 @@ apt update
 apt install -y crudini
 apt install -y mariadb-server
 
-crudini --set /etc/mysql/mariadb.conf.d/50-server.cnf mysqld bind-address 0.0.0.0
 
 mysql -e "create database glpi;"
-mysql -e "create user glpi@'%' identified by 'glpi';"
-mysql -e "grant all privileges on glpi.* to glpi@'%';"
-mysql -e "revoke all privileges on glpi.* from glpi@'%';"
-mysql -e "flush privileges;"
+mysql -e "create user glpi@'%' identified by 'glpi'; grant all privileges on glpi.* to glpi@'%'; revoke all privileges on glpi.* from glpi@'%'; flush privileges;"
+
+crudini --set /etc/mysql/mariadb.conf.d/50-server.cnf mysqld bind-address 0.0.0.0
 
 systemctl restart mariadb
