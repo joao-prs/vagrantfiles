6bfd977720625cd727b8b229c2ad1dabe0fb7075
joao <kandrocmatunein@gmail.com>
2023-06-02 14:56:57 -0300

    polimento ubuntu-other

--- ubuntu-other/initial.sh
+++ ubuntu-other/initial.sh
@@ -21,9 +21,9 @@ timedatectl set-timezone America/Belem
 #    (step not secure)     #
 #                          #
 ############################
-mkdir /home/vagrant/.ssh
 # id_rsa.pub
-cat >> /home/ansible/.ssh/authorized_keys << EOF
+cat >> /home/vagrant/.ssh/authorized_keys << EOF
+
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCZH8kcUcjW+OPchPAPYB8aBVjl3+NguVAejUzJJFm8NEJCTVYIx4P4wPV28PVDb7jeFUadQD6yO7sVbIWdfUad5w1hRs92hNaOzqtlpfPk9no2zJ+ueHgMqYkIfH7svlmr3wrzPFKmntTInjqa5cgBCIIefXcEXqVDnC8jof0O0Ai/Z7psL7L3+p3ZtQgvIhQENGrwP5IoEZ1vMQ+Mu1NpwP3IxfLe+PYClAWJQznBqcn98EPj6PLIpW91813WCCWublKBbGqk/mr+0B8pkyoxFNIgJYJttCTOVGEHczI2XAM9Zj3aFd3/S16aTxO1WSmoIFxJMZaUebaWTJ9YbLLWYV56XgZEoM1zH/A/mgAhzyZAQQC46/n9D/OroxDVjruuQ38p/n3jFpPxVaIYO0t8yHUprTtBQ6t49K1SXyoMJ9mavLRqqI3InOSvl2VTMaZk4mOO3ZVAyuL/Xejguvlv8rFgCZYLwRxecKAUBBI+GV15oLToRXCAowW45DqP3nwiM6X1s8TU1WHD405eziS2a/j3Ao9s2aFuDKePyd1wOM4U/q5afWilR4jOOCsu+aJhsuMDfZxoCWefj2HRrUuedl8YyvWx0Rsm9Tq1Ju1g6Wcz8cU94tlNTyw5l48zeiHwNsCnKWerEqwGj8+A5wPf+TliYOiZL0Gk1DS25LBU9Q== jpedro@aspire-5
 EOF
 
@@ -36,13 +36,11 @@ useradd -m ansible
 usermod -aG sudo ansible
 chsh -s /bin/bash ansible
 passwd -d ansible 
-chown ansible:ansible /var/lib/apt/lists/lock
-mkdir /home/ansible/.ssh/
-cp /home/ubuntu/.ssh/authorized_keys /home/ansible/.ssh/
+mkdir /home/ansible/.ssh
+cp /home/vagrant/.ssh/authorized_keys /home/ansible/.ssh/
 chown -R ansible:ansible /home/ansible/.ssh
 chmod 700 /home/ansible/.ssh
 chmod 600 /home/ansible/.ssh/authorized_keys
-
 
 
 # show ip when finish the process
