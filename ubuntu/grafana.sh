ecb67a0c4b0fa5ac4cb9795caf7d4fe9e2ca55b6
Joao <kandrocmatunein@gmail.com>
2023-07-17 18:26:12 -0300

    ubuntu

---
+++ ubuntu/grafana.sh
@@ -0,0 +1,16 @@
+
+sudo apt-get install -y apt-transport-https
+sudo apt-get install -y software-properties-common wget
+sudo wget -q -O /usr/share/keyrings/grafana.key https://apt.grafana.com/gpg.key
+echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com beta main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+sudo apt-get update
+sudo apt-get -y install grafana
+sudo apt-get -y install grafana-enterprise
+ip -br a
+sudo systemctl daemon-reload
+sudo systemctl start grafana-server
+sudo systemctl status grafana-server
+#http://ip:3000
+
+
